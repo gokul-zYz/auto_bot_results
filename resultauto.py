b57c9df3492bfbd9fe9ec4929186dc1b23bde9dd
GOKUL SUBRAMANIAN <96585296+GOKUL69@users.noreply.github.com>
2022-09-13 16:47:53 +0530

    Update resultauto.py

--- resultauto.py
+++ resultauto.py
@@ -5,8 +5,14 @@ import cv2;
 import textwrap;
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-#driver = webdriver.Chrome("E:\PYTHON\Chromederiver\chromedriver.exe")
-driver = webdriver.Chrome(ChromeDriverManager().install())
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
+
 driver.maximize_window()
 c="1"
 var=401
