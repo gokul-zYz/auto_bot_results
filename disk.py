90c914f81ba3d9aa20c48d4260379f49bdbbc242
GOKUL SUBRAMANIAN <96585296+GOKUL69@users.noreply.github.com>
2022-09-13 18:00:04 +0530

    Add files via upload

---
+++ disk.py
@@ -0,0 +1,11 @@
+from discord_webhook import DiscordWebhook,DiscordEmbed
+webhookurl="https://discord.com/api/webhooks/1019113357653065788/MUebVsWHRRfq-XYS2pDDAVyir5FuahBcMxGxS0ArNUsgj40UMcdS_1NWc2Cw9oCwF_gh"
+webhook=DiscordWebhook(url=webhookurl)
+embed=DiscordEmbed(title="------------------RESULT----------------",description="HELLO",color="ED4245")
+webhook.add_embed(embed)
+while 1>0:
+ for i in range(0,1000):
+    if i==1000:
+           webhook.execute()
+           print("message received")
+
