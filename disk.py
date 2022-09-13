dccff0c74528f9fc45738cd9ada1e129f0996e48
GOKUL SUBRAMANIAN <96585296+GOKUL69@users.noreply.github.com>
2022-09-14 00:20:49 +0530

    Update disk.py

--- disk.py
+++ disk.py
@@ -1,11 +1,14 @@
 from discord_webhook import DiscordWebhook,DiscordEmbed
 webhookurl="https://discord.com/api/webhooks/1019113357653065788/MUebVsWHRRfq-XYS2pDDAVyir5FuahBcMxGxS0ArNUsgj40UMcdS_1NWc2Cw9oCwF_gh"
 webhook=DiscordWebhook(url=webhookurl)
-embed=DiscordEmbed(title="------------------RESULT----------------",description="HEL23213LO",color="ED4245")
-webhook.add_embed(embed)
+
+c=1
 while 1>0:
+ c=c+1
  for i in range(0,1001):
     if i==1000:
-           webhook.execute()
-           print("message received")
+       embed=DiscordEmbed(title="------------------RESULT----------------",description=str(c)+"times",color="ED4245")
+       webhook.add_embed(embed)
+       webhook.execute()
+           
 
