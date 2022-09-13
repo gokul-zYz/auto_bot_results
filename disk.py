cc8d6cd82a2c73448596886671bd0d5d466ab405
GOKUL SUBRAMANIAN <96585296+GOKUL69@users.noreply.github.com>
2022-09-14 00:47:56 +0530

    Update disk.py

--- disk.py
+++ disk.py
@@ -1,14 +1,15 @@
 from discord_webhook import DiscordWebhook,DiscordEmbed
 webhookurl="https://discord.com/api/webhooks/1019113357653065788/MUebVsWHRRfq-XYS2pDDAVyir5FuahBcMxGxS0ArNUsgj40UMcdS_1NWc2Cw9oCwF_gh"
-webhook=DiscordWebhook(url=webhookurl)
+c=1
+embed=DiscordEmbed(title="BOT RUNNING",description="times",color="ED4245")
+webhook.add_embed(embed)
 
 
-while 1>0:
-
- for i in range(0,1001):
-    if i==500:
-       embed=DiscordEmbed(title="------------------RESULT----------------",description="times",color="ED4245")
-       webhook.add_embed(embed)
-       webhook.execute()
+while True:
+for i in range(0,3000):
+ if i==500:
+  webhook=DiscordWebhook(url=webhookurl,content=c+" "+TIMES")
+  webhook.execute()
+  c=c+1
            
 
